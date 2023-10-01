bea7cb79468b5887503e7252109255a76e64efef
justin <justin@lab.net>
2023-10-01 18:29:08 +1100

    Initial commit

---
+++ first_ec2.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "myec2" {
+       ami            = "ami-03a6eaae9938c858c"
+       instance_type  = "t2.micro"
+
+       tags = {
+         Name = "myec2"
+       }
+}
+
+
+
